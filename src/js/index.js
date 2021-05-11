074e5b8428faf5ea6452fd40f3b4eae8d57a7cd0
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-05-10 23:32:37 -0400

    first major breakthrough-MO

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,49 @@
 import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
+import { stores } from "./genre";
 
-const key = "YOURKEYHERE";
+const query = async function () {
+  try {
+    const response = await fetch(
+      "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=50&sortBy=Savings"
+    );
+    const data = await response.json();
+    data.forEach((games) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="games-card">
+      <div class="games-card-front">
+        <img
+          src="${games.thumb}"
+          alt=""
+          class="poster"
+        />
+      </div>
+      <div class="games-card-back">
+        <h3 class="games-card-header">${games.title}</h3>
+        <div class="price-box">
+          <p class="price">Get it now for:$ ${games.salePrice}</p>
+          <p class="price">Original Price:$ ${games.normalPrice}</p>
+         
+        </div>
+
+        <div class="score-box">
+          <p class="criticsTitle">Critics Ratings: ${games.metacriticScore}</p>
+         
+        </div>
+
+        <div class="savings-box">
+          <p class="savings">${Math.round(games.savings)}% off</p>
+         
+        </div>
+
+        
+      </div>
+    </div> `
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("Oops, something bad happened");
+  }
+};
+query();
