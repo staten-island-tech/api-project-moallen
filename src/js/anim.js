42fc6313eab4994a6af6971a9cf54e53cdf8f807
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-05-27 13:17:57 -0400

    idk why gsap scroll stopped working

--- src/js/anim.js
+++ src/js/anim.js
@@ -1,14 +1,14 @@
-const loader = document.querySelector('.loader');
-const main = document.querySelector('.main');
+const loader = document.querySelector(".loader");
+const main = document.querySelector(".main");
 
 function init() {
   setTimeout(() => {
     loader.style.opacity = 0;
-    loader.style.display = 'none';
+    loader.style.display = "none";
 
-    main.style.display = 'block';
+    main.style.display = "block";
     setTimeout(() => (main.style.opacity = 1), 50);
-  }, 4000);
+  }, 3000);
 }
 
 init();
